e364cd2b6407d5b8ce578ac3452c27bc57298574
Kevin Barnard <kbarnard@mbari.org>
2018-11-12 11:38:05 -0700

    Created Accelerometer class & updated tests

--- i2c_interface.py
+++ i2c_interface.py
@@ -2,13 +2,45 @@
 # Functions responsible for reading i2c from the RPi.
 from scipy.constants.constants import g
 import smbus
+import time
+
+class Accelerometer:
+    device = None
+    sample_times = []
+    sample_data = [[], [], []]
+
+    def __init__(self, device):
+        self.device = device
+
+    def read(self):
+        """ Returns a sample time and len=3 list of read accelerometer data. """
+        read_time = time.time()
+        read_data = self.normalize_data(self.device.read())
+        self.sample_times.append(read_time)
+        for i in range(3):
+            self.sample_data[i].append(read_data[i])
+        return read_time, read_data
+
+    def get_sample_times(self):
+        return self.sample_times
+
+    def get_axis_data(self, axis):
+        if axis in range(3):
+            return self.sample_data[axis]
+        else:
+            raise ValueError("Bad axis:", axis)
 
 
 class I2CDevice:
     address = None
+    bus = None
 
-    def __init__(self, device_number):
+    def __init__(self, bus, device_number):
         self.address = get_address(device_number)
+
+    def read(self):
+        """ Reads and returns a list of raw register data. """
+        return self.bus.read_i2c_block_data(self.address, 0x01, 6)
 
 
 class I2CBus:
@@ -23,7 +55,7 @@ class I2CBus:
     def init_device(self, device_number):
         """ Initializes an I2C device and adds it to device_dict. """
         device_address = get_address(device_number)
-        device = I2CDevice(device_number)  # Initialize device
+        device = I2CDevice(self, device_number)  # Initialize device
         self.bus.write_byte_data(device_address, 0x2A, 1)  # Initialize control register
         self.device_dict[device_number] = device  # Add device to dict
 
@@ -33,10 +65,27 @@ class I2CBus:
             raise ValueError("Out of bounds device number:", device_number)
         return self.device_dict[device_number]
 
-    def read_device(self, device_number):
-        """ Reads the specified device and returns its raw data. """
-        device = self.get_device(device_number)
-        return self.bus.read_i2c_block_data(device.address, 0x01, 6)
+
+def normalize_data(self, raw_data):
+    """ Converts raw accelerometer data to normalized values. """
+    data = [0, 0, 0]
+
+    # Convert to numbers
+    data[0] = (raw_data[0] << 4) + (raw_data[1] >> 4)
+    data[1] = (raw_data[2] << 4) + (raw_data[3] >> 4)
+    data[2] = (raw_data[4] << 4) + (raw_data[5] >> 4)
+
+    # Fix 2's complement numbers
+    for i in range(3):
+        if data[i] >> 11 == 1:
+            data[i] -= 2**12
+
+    return data
+
+
+def convert_datum(self, datum):
+    """ Converts normalized data list to acceleration data (m/s^2). """
+    return datum * 9.81 / 1024
 
 
 def get_address(device_number):
--- tests.py
+++ tests.py
@@ -1,17 +1,33 @@
 # tests.py
 # Tests for program functionality.
-import i2c_interface
+from i2c_interface import *
+import numpy as np
+import matplotlib.pyplot as plt
 
 
 def test_i2c():
     bus0 = I2CBus(0)
     bus1 = I2CBus(1)
-    bus0.init_device(0)
-    bus0.init_device(1)
+
     bus1.init_device(0)
-    acc0 = bus0.get_device(0)
-    acc1 = bus0.get_device(1)
-    acc2 = bus1.get_device(2)
+    bus1.init_device(1)
+    bus0.init_device(0)
+
+    acc0 = Accelerometer(bus1.get_device(0))
+    acc1 = Accelerometer(bus1.get_device(1))
+    acc2 = Accelerometer(bus0.get_device(0))
+
+    ti = time.time()
+    while time.time() - ti < 10:
+        acc0.read()
+        acc1.read()
+        acc2.read()
+
+    acc0_data_z = np.array(map(convert_datum, acc0.get_axis_data(2)))
+    acc0_sample_times = np.array(acc0.get_sample_times())
+    plt.plot(acc0_sample_times, acc0_data_z)
+    plt.show()
+
 
 if __name__ == "__main__":
     pass
