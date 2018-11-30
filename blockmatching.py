6fa4c66758c2ea2e507b90960b1162b6eb51ffbc
Kevin Barnard <kbarnard@mbari.org>
2018-11-30 10:50:44 -0700

    Updated blockmatching algorithm

--- blockmatching.py
+++ blockmatching.py
@@ -1,20 +1,11 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Nov 13 18:23:00 2018
-
-@author: brendanpaver
-"""
+# blockmatching.py
+# TDOA calculation
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def TDOA(accel_0, accel_1, timeArray):
     corr = np.correlate(accel_0,accel_1, "same")
-    max_index = np.argmax(corr)
-    print("max_index =", max_index)
+    index_offset = np.argmax(corr) - corr.size / 2
     avg_diff = np.mean(np.ediff1d(timeArray))
-    print("avg_diff =", avg_diff)
-    return avg_diff * max_index
-
-if __name__ == "__main__":
-    print(TDOA(accel_0,accel_1,timeArray))
+    return avg_diff * index_offset
