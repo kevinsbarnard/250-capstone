6b839f493e176ef6ee431887bcfa6240acf9d337
Kevin Barnard <kbarnard@mbari.org>
2018-11-30 11:01:47 -0700

    Minor fix in TDOA algorithm

--- blockmatching.py
+++ blockmatching.py
@@ -6,6 +6,6 @@ import matplotlib.pyplot as plt
 
 def TDOA(accel_0, accel_1, timeArray):
     corr = np.correlate(accel_0,accel_1, "same")
-    index_offset = np.argmax(corr) - corr.size / 2
+    index_offset = np.argmax(np.abs(corr)) - corr.size / 2
     avg_diff = np.mean(np.ediff1d(timeArray))
     return avg_diff * index_offset
